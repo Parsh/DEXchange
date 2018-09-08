fe616335b45e1466500377bf59cc65380518051c
Parsh24 <parsh.cosmos11@gmail.com>
2018-09-08 23:51:35 +0530

    General Structure and Balances

--- src/ethereum/contracts/Exchange.sol
+++ src/ethereum/contracts/Exchange.sol
@@ -4,9 +4,53 @@ import "./DEXtoken.sol";
 
 contract Exchange {
 
+    // GENERAL STRUCTURE //
+    
+    struct Offer {
+        uint amount;
+        address who;
+    }
+
+    struct OrderBook {
+        uint higherPrice;
+        uint lowerPrice;
+
+        mapping (uint => Offer) offers;
+
+        uint offers_key;
+        uint offers_lenght;
+    }
+
+    struct Token {
+        address tokenContract;
+        string symbolName;
+
+        mapping (uint => OrderBook) buyBook;
+
+        uint currentBuyPrice;
+        uint lowestBuyPrice;
+        uint amountBuyPrices;
+
+        mapping (uint => OrderBook) sellBook;
+        uint currentSellPrice;
+        uint highestSellPrice;
+        uint amountSellPrices;
+
+    }
+
+    // supporting a max of 255 tokens...
+    mapping (uint8 => Token) tokens;
+    uint8 tokenIndex;
+
+    // setting up the owner
     address public owner;
 
     constructor () public {
         owner = msg.sender;
     }
+
+    // BALANCES //
+    mapping (address => mapping (uint8 => uint)) tokenBalanceForAddress;
+    mapping (address => uint) balanceEthForAddress;
+
 }
