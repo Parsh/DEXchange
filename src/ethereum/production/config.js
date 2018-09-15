f803bbe556d1b391f5b70b1d3a8a8788b7719f45
Parsh24 <parsh.cosmos11@gmail.com>
2018-09-15 15:16:38 +0530

    deployment configurations

---
+++ src/ethereum/production/config.js
@@ -0,0 +1,9 @@
+const MNEMONIC =
+  'chief outside coast artefact enrich pelican raw top yellow witness slogan glide';
+const NETWORKURL =
+  'https://rinkeby.infura.io/v3/b4e2b3bf723f4985ae36bc838089e50d';
+
+module.exports = {
+  MNEMONIC,
+  NETWORKURL
+};
