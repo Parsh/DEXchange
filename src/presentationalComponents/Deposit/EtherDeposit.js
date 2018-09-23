59a1e28dfb30142854aedcf63f980f5b16868694
Parsh24 <parsh.cosmos11@gmail.com>
2018-09-23 22:13:43 +0530

    slight changes

--- src/presentationalComponents/Deposit/EtherDeposit.js
+++ src/presentationalComponents/Deposit/EtherDeposit.js
@@ -5,13 +5,13 @@ import { Input, Button, Fa } from 'mdbreact';
 const EtherDeposit = props => {
   return (
     <form>
-      <p className="h3-responsive text-center mb-4">Deposit Ether</p>
+      <p className="h4-responsive text-center mb-4">Deposit Ether</p>
       <div className="grey-text">
         <Input label="Amount in Ether" icon="user" group type="text" />
       </div>
       <div className="text-center">
         <Button outline color="info">
-          Deposit <Fa icon="paper-plane-o" className="ml-1" />
+          Deposit Ether <Fa icon="paper-plane-o" className="ml-1" />
         </Button>
       </div>
     </form>
--- src/presentationalComponents/Deposit/TokenDeposit.js
+++ src/presentationalComponents/Deposit/TokenDeposit.js
@@ -4,14 +4,14 @@ import { Input, Fa, Button } from 'mdbreact';
 const TokenDeposit = props => {
   return (
     <form>
-      <p className="h3-responsive text-center mb-4">Deposit Token</p>
+      <p className="h4-responsive text-center mb-4">Deposit Token</p>
       <div className="grey-text">
         <Input label="Token Symbol Name" icon="user" />
         <Input label="Number of Tokens" icon="user" group type="text" />
       </div>
       <div className="text-center">
         <Button outline color="info">
-          Deposit <Fa icon="paper-plane-o" className="ml-1" />
+          Deposit Token <Fa icon="paper-plane-o" className="ml-1" />
         </Button>
       </div>
     </form>
