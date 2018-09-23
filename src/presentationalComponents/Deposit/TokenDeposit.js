6703bcef43a56b6a7da16fdf17d24bf0d4ec181c
Parsh24 <parsh.cosmos11@gmail.com>
2018-09-23 21:20:04 +0530

    UI: Token Deposit

--- src/presentationalComponents/Deposit/TokenDeposit.js
+++ src/presentationalComponents/Deposit/TokenDeposit.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Input, Fa, Button } from 'mdbreact';
 
 const TokenDeposit = props => {
-  return <div>Token Deposit</div>;
+  return (
+    <form>
+      <p className="h3-responsive text-center mb-4">Deposit Token</p>
+      <div className="grey-text">
+        <Input label="Token Symbol Name" icon="user" />
+        <Input label="Number of Tokens" icon="user" group type="text" />
+      </div>
+      <div className="text-center">
+        <Button outline color="info">
+          Deposit <Fa icon="paper-plane-o" className="ml-1" />
+        </Button>
+      </div>
+    </form>
+  );
 };
 
 export default TokenDeposit;
