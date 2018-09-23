74f0d133edd437ed53cfb94bbf5e612cced2712d
Parsh24 <parsh.cosmos11@gmail.com>
2018-09-23 21:03:26 +0530

    UI: Ether Deposit

--- src/presentationalComponents/Deposit/EtherDeposit.js
+++ src/presentationalComponents/Deposit/EtherDeposit.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import { Input, Button, Fa } from 'mdbreact';
+
 const EtherDeposit = props => {
-  return <div>Ether Deposit</div>;
+  return (
+    <form>
+      <p className="h3 text-center mb-4">Deposit Ether</p>
+      <div className="grey-text">
+        <Input label="Amount in Ether" icon="user" group type="text" />
+      </div>
+      <div className="text-center">
+        <Button outline color="info">
+          Deposit <Fa icon="paper-plane-o" className="ml-1" />
+        </Button>
+      </div>
+    </form>
+  );
 };
 
 export default EtherDeposit;
